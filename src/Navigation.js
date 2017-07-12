4696085705b438dd8a303609a1134b744b6e01d7
Rodrigo Thauby <rthauby@gmail.com>
2017-07-12 13:24:13 -0400

    navigation work

--- src/Navigation.js
+++ src/Navigation.js
@@ -7,14 +7,35 @@ import Hobbies from './Hobbies'
 
 import './styles/Navigation.css';
 
+let navClass = 'closed';
+
 class Navigation extends Component {
+
+  handleMobileNavClick(e){
+    const el = e.currentTarget;
+    el.classList.toggle('open');
+
+    if(el.classList.contains('open')){
+      navClass = 'open';
+    } else {
+      navClass = 'closed';
+    }
+  }
+
   render() {
     return (
       <nav className="navigation">
           <div className="row aligned-center">
               <div className="small-12 columns">
                   <h1><a href="/" className="home-link">Rodrigo Thauby</a></h1>
-                  <ul className="navigation__list">
+
+                  <div id="nav-icon" onClick={e => this.handleMobileNavClick(e)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+
+                  <ul className={`navigation__list ${navClass}`}>
                       <li className="nav-list__item"><NavLink to="/" activeClassName="selected">Home</NavLink></li>
                       <li className="nav-list__item"><NavLink to="/work" activeClassName="selected">Work</NavLink></li>
                       <li className="nav-list__item"><NavLink to="/hobbies" activeClassName="selected">Hobbies</NavLink></li>
