03dabefe400c9129117cda731913f0af2fb32e0b
Rodrigo Thauby <rthauby@gmail.com>
2017-07-17 15:56:35 -0400

    finished nav

--- src/Navigation.js
+++ src/Navigation.js
@@ -7,19 +7,30 @@ import Hobbies from './Hobbies'
 
 import './styles/Navigation.css';
 
-let navClass = 'closed';
 
 class Navigation extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      navClass : 'closed'
+    }
+  }
 
   handleMobileNavClick(e){
     const el = e.currentTarget;
     el.classList.toggle('open');
 
     if(el.classList.contains('open')){
-      navClass = 'open';
+      this.setState({'navClass':'open'});
     } else {
-      navClass = 'closed';
+      this.setState({'navClass':'closed'});
     }
+  }
+
+  handleCloseNav(){
+    this.setState({'navClass':'closed'});
   }
 
   render() {
@@ -35,10 +46,10 @@ class Navigation extends Component {
                     <span></span>
                   </div>
 
-                  <ul className={`navigation__list ${navClass}`}>
-                      <li className="nav-list__item"><NavLink to="/" activeClassName="selected">Home</NavLink></li>
-                      <li className="nav-list__item"><NavLink to="/work" activeClassName="selected">Work</NavLink></li>
-                      <li className="nav-list__item"><NavLink to="/hobbies" activeClassName="selected">Hobbies</NavLink></li>
+                  <ul className={`navigation__list ${this.state.navClass}`}>
+                      <li className="nav-list__item"><NavLink to="/" activeClassName="selected" onClick={this.handleCloseNav}>Home</NavLink></li>
+                      <li className="nav-list__item"><NavLink to="/work" activeClassName="selected" onClick={this.handleCloseNav}>Work</NavLink></li>
+                      <li className="nav-list__item"><NavLink to="/hobbies" activeClassName="selected" onClick={this.handleCloseNav}>Hobbies</NavLink></li>
                       <li className="nav-list__item"><a href="https://medium.com/@Pucho">Blog</a></li>
                   </ul>
               </div>
